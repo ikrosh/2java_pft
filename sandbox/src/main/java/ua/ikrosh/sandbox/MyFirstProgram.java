6ec123e423be58f469008b67f91de12908f1b790
ikrosh <ikroshkina@gmail.com>
2017-04-01 18:49:02 +0300

    L1.15 - при расчете площадей функции ассоциированы с объектами

--- sandbox/src/main/java/ua/ikrosh/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/ikrosh/sandbox/MyFirstProgram.java
@@ -8,23 +8,16 @@ public class MyFirstProgram {
 		hello("Iren");
 
 		Square s = new Square(5);
-		System.out.println("Площадь квадрата с стороной " + s.l + " = " + area(s));
+		System.out.println("Площадь квадрата с стороной " + s.l + " = " + s.area());
 
 		Rectangle r = new Rectangle(4,6);
-		System.out.println("Площадь прямоугольника с сторонами " + r.a + " и " + r.b + " = " + area(r));
+		System.out.println("Площадь прямоугольника с сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
-
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!!!");
 	}
 
-	private static double area(Square s) {
-		return s.l * s.l;
-	}
 
-	private static double area(Rectangle r) {
-		return r.a * r.b;
-	}
 
 }
--- sandbox/src/main/java/ua/ikrosh/sandbox/Rectangle.java
+++ sandbox/src/main/java/ua/ikrosh/sandbox/Rectangle.java
@@ -10,4 +10,7 @@ public class Rectangle {
     this.b = b;
   }
 
+  public double area() {
+    return this.a * this.b;
+  }
 }
