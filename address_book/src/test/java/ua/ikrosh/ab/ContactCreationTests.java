3a3099ec485cffaa1eea24088c81705fd3dff5b6
ikrosh <ikroshkina@gmail.com>
2017-04-01 22:18:37 +0300

    L2.7 - Задание 4 -  Рефакторинг тестов создания контактов
    Выделение вспомогательных методов, параметров, классов

--- address_book/src/test/java/ua/ikrosh/ab/ContactCreationTests.java
+++ address_book/src/test/java/ua/ikrosh/ab/ContactCreationTests.java
@@ -3,16 +3,11 @@ package ua.ikrosh.ab;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class ContactCreationTests {
     FirefoxDriver wd;
@@ -22,32 +17,57 @@ public class ContactCreationTests {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String user, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(user);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
-    
+
     @Test
     public void testContactCreation () {
-        wd.findElement(By.linkText("home")).click();
-        wd.findElement(By.linkText("add new")).click();
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("tttt");
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("dddd");
-        wd.findElement(By.name("middlename")).click();
-        wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys("gggg");
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+        initContactCreation();
+        fillContactForm(new ContactData("test1", "test2", "test3", "test@gmail.com", "3456"));
+        submitContactCreation();
+        returnToHomePage();
+    }
+
+    private void returnToHomePage() {
         wd.findElement(By.linkText("home")).click();
     }
-    
+
+    private void submitContactCreation() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys(contactData.getHomePhone());
+    }
+
+    private void initContactCreation() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
---
+++ address_book/src/test/java/ua/ikrosh/ab/ContactData.java
@@ -0,0 +1,37 @@
+package ua.ikrosh.ab;
+
+public class ContactData {
+  private final String lastname;
+  private final String firstname;
+  private final String address;
+  private final String email;
+  private final String homePhone;
+
+  public ContactData(String lastname, String firstname, String address, String email, String homePhone) {
+    this.lastname = lastname;
+    this.firstname = firstname;
+    this.address = address;
+    this.email = email;
+    this.homePhone = homePhone;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getHomePhone() {
+    return homePhone;
+  }
+}
