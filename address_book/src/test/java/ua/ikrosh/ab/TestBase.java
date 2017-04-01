f10607e443a86fbe49bdd6444accc5a4c3af2089
ikrosh <ikroshkina@gmail.com>
2017-04-02 00:00:22 +0300

    L2.8 - coздан класс TestBase и в него перенесены данные из тестов для контактов

--- address_book/src/test/java/ua/ikrosh/ab/TestBase.java
+++ address_book/src/test/java/ua/ikrosh/ab/TestBase.java
@@ -91,4 +91,60 @@ public class TestBase {
     }
     wd.findElement(By.name("edit")).click();
   }
+
+  protected void returnToHomePage() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  protected void submitContactCreation() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  protected void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactData.getHomePhone());
+  }
+
+  protected void initContactCreation() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  public void conformContactDeletion() {
+    wd.switchTo().alert().accept();
+  }
+
+  protected void submitContactDeletion() {
+    wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
+  }
+
+  protected void initContactDeletion() {
+    if (!wd.findElement(By.name("selected[]")).isSelected()) {
+            wd.findElement(By.name("selected[]")).click();
+        }
+  }
+
+  protected void gotoHomePage() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  protected void submitContactModification() {
+    wd.findElement(By.xpath("//div[@id='content']/form[1]/input[22]")).click();
+  }
+
+  protected void initContactModification() {
+    wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[5]/td[8]/a/img")).click();
+  }
 }
