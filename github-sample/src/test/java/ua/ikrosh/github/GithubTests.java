7aaa3433077d55604b6ddcf5af1d3e7803e8f6e2
ikrosh <ikroshkina@gmail.com>
2017-04-17 01:00:15 +0300

    L9.6 - REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ua/ikrosh/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ua.ikrosh.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("37d5ba7c1d4dab857382f55652881320d79c4632");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("ikrosh", "2java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
