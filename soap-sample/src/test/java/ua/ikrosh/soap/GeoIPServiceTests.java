72ac93f735459ef54897386fe0202535958187db
ikrosh <ikroshkina@gmail.com>
2017-04-16 21:00:15 +0300

    L9.2 - SOAP: простой пример

---
+++ soap-sample/src/test/java/ua/ikrosh/soap/GeoIPServiceTests.java
@@ -0,0 +1,23 @@
+package ua.ikrosh.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIPServiceTests {
+
+  @Test
+  public void testMyIp() {
+    GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("194.28.29.152");
+    assertEquals(geoIP.getCountryCode(), "RUS");
+  }
+
+  @Test
+  public void testInvalidIp() {
+    GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("185.150.154.xxx");
+    assertEquals(geoIP.getCountryCode(), "RUS");
+  }
+
+}
