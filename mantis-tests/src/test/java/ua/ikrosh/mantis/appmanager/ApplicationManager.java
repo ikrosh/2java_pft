a0956fcbc8e496bc11ea70ff60be93c741dc30a0
ikrosh <ikroshkina@gmail.com>
2017-04-16 16:17:08 +0300

    L8.6 - Получение почты на встроенный в тесты почтовый сервер
    + Добавление HelperBase

--- mantis-tests/src/test/java/ua/ikrosh/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ua/ikrosh/mantis/appmanager/ApplicationManager.java
@@ -21,6 +21,7 @@ public class ApplicationManager {
   private String browser;
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
+  private MailHelper mailHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -74,4 +75,13 @@ public class ApplicationManager {
     }
     return wd;
   }
+
+  public MailHelper mail() {
+    if (mailHelper == null) {
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
+  }
+
+
 }
